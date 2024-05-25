2972f62c28e65c14c00ffe9d11011ad16aa6e9eb
txr-nicole <nicolefagundes24@gmail.com>
2024-05-25 15:53:54 -0300

    🚧 criando requisição do login

--- public/js/login.js
+++ public/js/login.js
@@ -1,16 +1,11 @@
-function fazerLogin(){
-    let email = document.getElementById("email").value
-    let senha = document.getElementById("senha").value
-    let banco = localStorage.getItem("Banco");
-    banco = JSON.parse(banco);
-    
-    for (let i = 0; i < banco.length; i++) {
-        if (banco[i].dadosLogin.email == email && banco[i].dadosLogin.senha == senha) {
-            sessionStorage.setItem("LoginAtual", JSON.stringify(banco[i]));
-            alert("Bem-vindo")
-        }
-    }
-    
-
+function fazerLogin() {
+  buscarDados()
 }
-
+const baseURL = window.location.href;
+async function buscarDados(){
+ const res = await fetch(baseURL, {
+  method: "GET"
+ })
+ console.log(res)
+ 
+}
