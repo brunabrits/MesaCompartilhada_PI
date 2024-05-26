489f8e93f7a4ba828662a7978646e7e49e62affe
Bruna Brito <brunabsilva0409@gmail.com>
2024-05-26 15:57:53 -0300

    🗃️ script banco de dados

---
+++ src/Mesa_CompartilhadaDB.sql
@@ -0,0 +1,60 @@
+CREATE DATABASE MESA_COMPARTILHADA
+
+CREATE TABLE ENDERECO_ED (
+  Endereco_Ed varchar(80) PRIMARY KEY,
+  Logradouro_Ed varchar(50),
+  Numero_Ed char(6),
+  Cep_Ed char(8),
+  Complemento_Ed varchar(20),
+  Cidade_Ed varchar(20),
+  Bairro_Ed varchar(20)
+)
+
+CREATE TABLE ENDERECO_ER(
+  Endereco_Er varchar(80) PRIMARY KEY,
+  Logradouro_Er varchar(50),
+  Numero_Er char(6),
+  Cep_Er char(8),
+  Complemento_Er varchar(20),
+  Cidade_Er varchar(20),
+  Bairro_Er varchar(20)
+)
+
+CREATE TABLE EMPRESA_DOADORA(
+ Cod_Ed int not null PRIMARY KEY,
+ Nome_Ed varchar(50),
+ Cnpj_Ed char(14),
+ Tipo_Ed varchar(20),
+ Email_Ed varchar(30),
+ Senha_Ed varchar(20),
+ Status_Ed varchar(7),
+ Endereco_Ed varchar(80),
+ FOREIGN key (Endereco_Ed) REFERENCES ENDERECO_ED(Endereco_Ed)
+)
+
+
+CREATE TABLE EMPRESA_RECEBEDORA(
+ Cod_Er int not null PRIMARY KEY,
+ Nome_Er varchar(50),
+ Cnpj_Er char(14),
+ Tipo_Er varchar(20),
+ Email_Er varchar(30),
+ Senha_Er varchar(20),
+ Status_Er varchar(7),
+ Endereco_Er varchar(80),
+ FOREIGN key (Endereco_Er) REFERENCES ENDERECO_Er(Endereco_Er)
+)
+
+CREATE TABLE DOACAO(
+ Cod_Doacao int not null PRIMARY KEY,
+ Nome_Doacao varchar(50),
+ Desc_Doacao varchar(30),
+ Status_Doacao varchar(10),
+ Observacao_Doacao varchar(30),
+ Data_Postada_Doacao datetime,
+ Data_Encerrada_Doacao datetime,
+ Cod_Ed int,
+ Cod_Er int,
+ FOREIGN KEY (Cod_Ed) REFERENCES EMPRESA_DOADORA(Cod_Ed),
+ FOREIGN KEY (Cod_Er) REFERENCES EMPRESA_RECEBEDORA(Cod_Er)
+  )
