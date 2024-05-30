e57e99460b9c9bacc76bf9d1eadf3d436c1dbfe0
txr-nicole <nicolefagundes24@gmail.com>
2024-05-30 19:55:01 -0300

    ✨ faz o login funcionar

--- public/js/login.js
+++ public/js/login.js
@@ -1,18 +1,40 @@
+function pegaDados() {
+  let email = document.getElementById("email").value
+  let senha = document.getElementById("senha").value;
+  let ele = document.getElementsByName("empresa");
+  let tipoclicado;
+  for (i = 0; i < ele.length; i++) {
+    if (ele[i].checked) {
+      tipoclicado = ele[i].id;
+    }
+  }
+
+
+  return {
+    email: email,
+    senha: senha,
+    tipo: tipoclicado,
+  };
+  }
+
+
+
 function fazerLogin() {
-  buscarDados()
+  buscarDados();
 }
 const baseURL = window.location.href;
-async function buscarDados(){
- const res = await fetch(baseURL, {
-  method: "POST",
-  headers: {
-    "Content-Type": "application/json",
-  },
-  body: JSON.stringify({
-    email: document.getElementById("email").value,
-    senha: document.getElementById("senha").value,
-  }),
- })
+async function buscarDados() {
+  const res = await fetch(baseURL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(pegaDados()),
+  });
   const data = await res.json();
- 
+  if (data.data){
+    sessionStorage.setItem("usuario", JSON.stringify(data.data));
+    window.location.href = "/dashboard";
+  }
+  alert(data.message);
 }
--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,11 +1,34 @@
+const dao = require("../database/dao");
+
 const loginG = (req, res) => {
   res.render("pages/login");
 };
 
 const loginP = (req, res) => {
-  const { email, senha } = req.body;
-  console.log(email);
-  console.log(senha);
+  const { email, senha, tipo } = req.body;
+  if (tipo == "doadora"){
+    dao.getEDByLogin(email, senha, (results) => {
+      console.log(results);
+    if (!results){
+      res.json({message: "Email ou senha incorretos"});
+    }else {
+      let sessiondata = {cod: results.Cod_Ed, nome: results.Nome_Ed, tipo: tipo}
+      res.json({message: "Usuário encontrado!", data: sessiondata});
+    }
+    });
+  } else if (tipo == "recebedora"){
+    dao.getERByLogin(email, senha, (results) => {
+      console.log(results);
+    if (!results){
+      res.json({message: "Email ou senha incorretos"});
+    }else {
+      let sessiondata = {cod: results.Cod_Er, nome: results.Nome_Er, tipo: tipo}
+      res.json({message: "Usuário encontrado!", data: sessiondata});
+    }
+    });
+  } else {
+    res.json({message: "Tipo de empresa inválido"});
+  }
 }
 
 module.exports = {
