7947207fae43376116a43c1dd9f74d57bd6f78fd
txr-nicole <nicolefagundes24@gmail.com>
2024-06-01 17:12:21 -0300

    🚧 listagem das doações

--- src/routes/plataforma.js
+++ src/routes/plataforma.js
@@ -1,9 +1,15 @@
 const { Router } = require("express");
+const dao = require("../database/dao");
 const path = require("path");
 const router = Router();
 
 router.get("/dashboard", (req, res) => {
-  res.render("pages/dashboard")
+  dao.getAllDoacoes((doacoes) => {
+    res.render("pages/dashboard", {
+      faltaDoacoes: doacoes.length === 0,
+      doacoes: doacoes.length > 0 ? doacoes : null,
+    });
+  })
 });
 
 router.get("/meusdados", (req, res) => {
